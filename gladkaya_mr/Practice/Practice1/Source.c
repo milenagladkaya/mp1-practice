490a6106a244990928e79dcb89c9f76dac640271
milenagladkaya <43129552+milenagladkaya@users.noreply.github.com>
2018-11-12 10:35:57 +0300

    practice1

---
+++ gladkaya_mr/Practice/Practice1/Source.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+void main(){
+
+	float pDVP, pDSP, wood, w, h, d;
+	setlocale(LC_ALL, "Russian");
+	printf("Введите плотность ДВП в кг/м^3");
+	scanf("%f", &pDVP);
+	printf("Введите плотность ДCП в кг/м^3");
+	scanf("%f", &pDSP);
+	printf("Введите плотность дерева в кг/м^3");
+	scanf("%f", &wood);
+
+
+
+
+		
